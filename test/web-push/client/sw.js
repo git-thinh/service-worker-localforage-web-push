82f170c663fd266448a5230b3b8ef0d382cd3a80
nguyenvanthinh <nguyenvanthinh@f88.vn>
2019-10-28 09:20:04 +0700

    update

--- test/web-push/client/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', async event => {
-
-    const data = await event.data.json();
-
-    console.log('SW -> PUSH = ', data);
-
-
-    //self.registration.showNotification(data.title, { body: 'Yay it works!' });
-});
